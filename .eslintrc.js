a27dda494f34bb6c0f0c3b10c39739d2f48716ef
Sergey Kabardinov <greenabomination@gmail.com>
2019-07-05 15:30:40 +0500

    drawing field

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
-    "extends": "google",
-    "installedESLint": true
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ]
 };
--- snake.js
+++ snake.js
@@ -1,12 +1,40 @@
+const FIELD_WIDTH = 100;
+const FIELD_HEIGHT = 50;
+const START_SPEED = 500;
+
 class game {
-    constructor(gameContainer, speed) {
-        const FIELD_WIDTH = 100;
-        const FIELD_HEIGHT = 100;
-        this.speed = speed; // game speed in ms
+    constructor(gameContainer, startSpeed) {
+
+        this.lvl = 0;
+        this.speed = startSpeed; // game speed in ms
         this.score = 0;
         this.gameContainer = gameContainer;
-        this.field = new gameField(this.gameContainer, FIELD_WIDTH, FIELD_HEIGHT);
-        console.log(this);
+        this.gameContainer.innerHTML = '';
+        this.drawHeader();
+        this.drawInfo();
+        this.drawGameField();
+        // setInterval(() => {
+        //     this.info.refresh(++this.score, --this.lvl);
+        // }, 500);
+    }
+
+    drawHeader = function() {
+        this.header = document.createElement('h1');
+        this.gameContainer.appendChild(this.header);
+        this.header.textContent = 'SIMPLE SNAKE GAME. Enjoy =)';
+    }
+
+    drawGameField = function() {
+        this.fieldElement = document.createElement('div');
+        this.fieldElement.classList.add('field');
+        this.gameContainer.appendChild(this.fieldElement);
+        this.field = new gameField(this.fieldElement, FIELD_WIDTH, FIELD_HEIGHT);
+        this.field.drawField();
+    }
+
+    drawInfo = function() {
+        this.info = new information(this.gameContainer);
+        this.info.refresh(this.score, this.lvl);
     }
 }
 
@@ -15,12 +43,33 @@ class gameField {
         this.element = element;
         this.width = width;
         this.height = height;
+        this.field = [];
+    }
+
+    drawField = function() {
+        // this.element.textContent = "";
+        for (let i = 0; i < this.height; i++) {
+            this.field[i] = [];
+            let el = document.createElement("div");
+            el.classList.add("row");
+            this.element.appendChild(el);
+            for (let j = 0; j < this.width; j++) {
+                let cellElement = document.createElement("div");
+                cellElement.classList.add("cell");
+                el.appendChild(cellElement);
+                this.field[i][j] = new cell(cellElement);
+            }
+        }
     }
 }
 
 class cell {
-    constructor() {
-        console.log('create cell');
+    constructor(element) {
+        this.element = element;
+    }
+
+    clearCell = function() {
+        this.element.className = '';
     }
 }
 
@@ -36,4 +85,21 @@ class food {
     }
 }
 
-const snakeGame = new game(document.querySelector('body'), 500);
+class information {
+    constructor(hostElement) {
+        this.element = document.createElement('div');
+        this.score = document.createElement('span');
+        this.level = document.createElement('span');
+        hostElement.appendChild(this.element);
+        this.element.classList.add('score');
+        this.element.appendChild(this.score);
+        this.element.appendChild(document.createElement('br'));
+        this.element.appendChild(this.level);
+    }
+
+    refresh = function(score, lvl) {
+        this.score.innerHTML = `Score: ${score}`;
+        this.level.innerHTML = `Level: ${lvl}`;
+    }
+}
+const snakeGame = new game(document.querySelector('#host'), START_SPEED);
